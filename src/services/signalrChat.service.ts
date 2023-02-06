ab486688015433339aa627e5bb4024ebe4a0c4cc
2romanio005 <106441269+2romanio005@users.noreply.github.com>
2023-02-06 15:37:51 +0300

    обработка принятых сообщений

--- src/services/signalrChat.service.ts
+++ src/services/signalrChat.service.ts
@@ -1,6 +1,6 @@
 import { type HubConnection, HubConnectionBuilder, HubConnectionState, LogLevel } from "@microsoft/signalr";
 import { useAuthStore } from "@/stores/auth.store";
-import type { User, ChatMessage, Message } from "@/models";
+import { User, ChatMessage, Message } from "@/models";
 
 export type ChatInfo = {
     user: User;
@@ -14,7 +14,7 @@ export type MessageWithSender = {
 };
 export class SignalrChatService {
     public hubConnection: HubConnection = this.createConnection();
-    public chats: Map<string, Message[]>; // TODO: Структура что-бы хранить чаты.
+    public chats: {chat_name: string, message: Message[]}[]; // TODO: Структура что-бы хранить чаты.
 
     constructor() {
         this.chats = new Map<string, Message[]>();
@@ -35,6 +35,21 @@ export class SignalrChatService {
 
     private registerOnServerEvents() {
         this.hubConnection.on("OnConnect", (chats: ChatInfo[]) => {
+            for(var i = 0; i < chats.length; ++i){
+                this.chats.push({chat_name: chats[i].user.fullName, message: [] as Message[]});
+                var form = 0, to = 0
+                var arr_from = chats[i].messagesFromMe, arr_to = chats[i].messagesToMe;
+                while(from < arr_from.length && to < arr_to.length){
+                    if(arr_from[from].sendTime.getTime() < arr_to[to].sendTime.getTime()){
+                        this.chats[i].message.push(arr_from[from].body);
+                        from++;
+                    }else{
+                        this.chats[i].message.push(arr_to[from].body);
+                        to++;
+                    }
+
+                }
+            }
             // TODO: Распаковка чатов при соединении.
         });
 
